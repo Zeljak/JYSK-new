33b588b0adec3df2131514a9b62e20e8368dadb3
DaliborA <d.arula@adriamedia.hr>
2021-10-20 16:08:19 +0200

    CSS bugfixes

--- _javascript/main.js
+++ _javascript/main.js
@@ -31,3 +31,21 @@ document.querySelector('#submit-form').addEventListener('click', function(e){
   }
 
 });
+
+// Anketa Smooth Scroll
+;(function($){
+    $('#anketa__button').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+    
+            const hash = this.hash;
+    
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top - 70
+                },
+                2000
+            );
+        }
+    });
+})(jQuery);
